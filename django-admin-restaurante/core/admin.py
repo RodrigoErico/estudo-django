34df23563d20e875507aa9cf9d91c8cd805c77f0
Rodrio Erico <rodrigoerico25@gmail.com>
2022-12-03 04:02:30 -0400

    created class for list display

--- django-admin-restaurante/core/admin.py
+++ django-admin-restaurante/core/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description', 'category')

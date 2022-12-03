5dc21f1115f0f48d4e83eff07f0af28b87d05f0c
Rodrio Erico <rodrigoerico25@gmail.com>
2022-12-03 03:25:08 -0400

    created models database

--- django-admin-restaurante/core/models.py
+++ django-admin-restaurante/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)

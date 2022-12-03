2eea51e622d9a0db4b953e5c3390957ede8a8d3d
Rodrio Erico <rodrigoerico25@gmail.com>
2022-12-03 03:40:03 -0400

    str method

--- django-admin-restaurante/core/models.py
+++ django-admin-restaurante/core/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.name
